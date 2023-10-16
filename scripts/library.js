4d55bbb2a53474637ca76381fdf77f68262ac2fc
Nolan Gajdascz <78930672+Gajdascz@users.noreply.github.com>
2023-10-16 12:59:39 -0400

    Create form processing logic
    Added DOM selectors and form input processing to create Book object
    and push to library array.

--- scripts/library.js
+++ scripts/library.js
@@ -1,14 +1,12 @@
-// DOM Elements and Listeners
+// DOM Element Selectors
 const addBookButton = document.querySelector(`.add-book-button`);
 const addBookDialog = document.querySelector(`#add-book-dialog`);
+const addBookForm = document.querySelector(`#add-book-form`);
 const closeBookFormButton = document.querySelector(`#close-button`);
 addBookButton.addEventListener(`click`, (e) => {
   addBookDialog.showModal();
 });
 
-closeBookFormButton.addEventListener(`click`, (e) => {
-  addBookDialog.close();
-});
 
 
 // Variable Definitions
@@ -22,9 +20,24 @@ function Book(title, author, pages, read, ) {
   this.pages = pages;
   this.read = Boolean(read);
 }
-
-
 // Functions
 function addBookToLibrary() {
-
+  myLibrary.forEach(book => {
+    console.log(book);
+  });
 }
+
+// Listeners
+closeBookFormButton.addEventListener(`click`, (e) => {
+  addBookDialog.close();
+});
+
+addBookDialog.addEventListener(`submit`, () => {
+  let title = document.querySelector(`input#book-title`).value;
+  let author = document.querySelector(`input#book-author`).value;
+  let pages = document.querySelector(`input#book-pages`).value;
+  let read = () => document.querySelector(`#book-read`).checked ? true : false;
+  myLibrary.push(new Book(title, author, pages, read));
+  addBookToLibrary();
+  addBookForm.reset();
+});
